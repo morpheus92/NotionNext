2bbc0295854c17e21bf3034b14ae5096d6d73884
morpheus92 <140181961+morpheus92@users.noreply.github.com>
2023-10-17 22:34:44 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,8 +17,8 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'mengzhen', // 您的昵称
-  BIO: process.env.NEXT_PUBLIC_BIO || '', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '', // 您的昵称
+  BIO: process.env.NEXT_PUBLIC_BIO || '这里只分享基于经验的内容、经过测试的策略和学习驱动的信息', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.prosesynth.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, AI绘画, promot', // 网站关键词 英文逗号隔开
 
